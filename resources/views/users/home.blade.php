5c759ab00df8f37d48bbf2cb1033ca55084940d2
unknown <school@kredo.jp>
2023-08-03 12:30:23 +0800

    added

---
+++ resources/views/users/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-8 bg-warning">
+            Posts
+        </div>
+        <div class="col-4 bg-secondary">
+            {{-- profile overview --}}
+            Profile overview
+
+            {{-- suggested users --}}
+            Suggested Users
+        </div>
+    </div>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+
+Auth::routes();
+
+
+
+Route::group(["middleware" => "auth"], function () {
+    Route::get('/', [HomeController::class, 'index'])->name('index');
+    Route::get('/posts/create',[PostController::class,'create'])->name('post.create');
+});
