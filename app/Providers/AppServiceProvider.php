e1ac5c3f0961e6fb1f91eba2f190771bd4366d8b
Kurt John <kredoteacher.kurt@gmail.com>
2024-08-30 20:35:19 +0800

    added safe code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,6 +12,10 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
